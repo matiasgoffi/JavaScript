97a625da324d261c594845e3a2ed7be3b77d7364
Matías <goffimatias@gmail.com>
2022-05-04 17:47:23 -0300

    primeros pasos en un simulador de compra para blog personal

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-
-
+/*clase servicios con función constructora con  los datos del servicio */
 class servicios {
-    constructor(servicio){
-        this.servicio = servicio.servicio.toLowerCase();
-        this.dia = servicio.dia.toLowerCase();
-        this.precio = parseInt(servicio.precio);
+    constructor (servicio) {
+  this.servicio = servicio.servicio.toLowerCase();
+  this.dia = servicio.dia.toLowerCase();
+  this.precio = parseInt(servicio.precio);
     }
                                                     
 
 }
-/*funcion para agregar un nuevo servicio*/
+/*funcion para agregar un nuevo servicio al array de precios ingresando los datos por prompt*/
 function agregarServicio () {
     let nuevo_servicio = new servicios ({servicio:prompt("ingrese el tipo de servicio: "), dia:prompt("ingrese el dia que se encuentra disponible: "), precio:prompt("ingrese el precio: ")})
     
@@ -18,29 +17,29 @@ function agregarServicio () {
 
 }
 
-/*array que almacena los servicios */
+/*array de objetos que almacena los servicios  */
 
-const precios = [{servicio: "evento", dia: "lunes", precio: 2500},
-                    {servicio: "sesionIndividual", dia: "lunes", precio: 2000},
+let precios = [{servicio: "evento", dia: "lunes", precio: 2500},
+                    {servicio: "sesion Individual", dia: "lunes", precio: 2000},
                     { servicio: "evento", dia: "martes", precio: 2500},
-                    {servicio: "sesionIndividual", dia: "martes", precio: 2000},
+                    {servicio: "sesion Individual", dia: "martes", precio: 2000},
                     {servicio: "evento", dia: "miercoles", precio: 2500},
-                    {servicio: "sesionIndividual", dia: "miercoles", precio: 2000},
+                    {servicio: "sesion Individual", dia: "miercoles", precio: 2000},
                     {servicio: "evento", dia: "jueves", precio: 2500},
-                    {servicio: "sesionIndividual", dia: "jueves", precio: 2000},
+                    {servicio: "sesion Individual", dia: "jueves", precio: 2000},
                     {servicio: "evento", dia: "viernes", precio: 5000},
-                    {servicio: "sesionIndividual", dia: "viernes", precio: 4000},
-                    {servicio: "evento", dia: "sabado", precio: 5000},
-                    {servicio: "sesionIndividual", dia: "sabado", precio: 4000},
+                    {servicio: "sesion Individual", dia: "viernes", precio: 4000},
+                    {servicio: "evento", dia: "sabado", precio: 4000},
+                    {servicio: "sesion Individual", dia: "sabado", precio: 4000},
                     {servicio: "evento", dia: "domingo", precio: 5000},
-                    {servicio: "sesionIndividual", dia: "domingo", precio: 4000},
+                    {servicio: "sesion Individual", dia: "domingo", precio: 4000},
                     {servicio: "evento", dia: "feriado", precio: 6000},
-                    {servicio: "sesionIndividual", dia: "feriado", precio: 4500},                                   
+                    {servicio: "sesion Individual", dia: "feriado", precio: 4500},                                   
                 
                     
-                ]
+                ];
 
-                
+/*clase con función constructora que rehune los datos necesarios de un posible consultante (persona)*/
 
  class consultante {
     constructor (persona) {
@@ -53,6 +52,7 @@ const precios = [{servicio: "evento", dia: "lunes", precio: 2500},
     }
 }
 
+/*funcion que me permite solicitar una consulta, ingresando datos por prompt */
 function solicitarConsulta () {
     let nueva_consulta = new consultante ({nombre:prompt("ingrese su nombre: "), apellido:prompt("ingrese su apellido:"), ciudad:prompt("ingrese su ciudad:"), 
     provincia: prompt("ingrese su provincia:"), zip:prompt("ingrese su codigo postal:"),  email:prompt("ingrese su email:")});
@@ -63,11 +63,11 @@ function solicitarConsulta () {
 
 }
 
-/*array para guardar las consultas */
-const consultantes = [];
+/*array para guardar las consultas, lo veo últil para utilizar en el proceso más adelante */
+let consultantes = [];
  
                           
-
+/*funcion para mostrar los servicios y poder seleccionar el que sea desea comprar */
 function mostrarServicios() {
     let salida = "";
     let i = 1;
@@ -81,60 +81,64 @@ function mostrarServicios() {
 
 }
 
+/*función para comprar uno o más servicios, mostrarlos por consola y  agregarlos a un carrito de compras */
 
 function comprarServicio () {
     let salir = "";
 
-    while (salir != "ESC" || salir != "esc" || salir !="" || salir != null){
+    while (salir != "ESC"){
     let lista_servicios = mostrarServicios();
     let servicio_seleccionado = parseInt(prompt("ingrese el numero del servicio que desea contratar: \n\n" + lista_servicios))
     let servicio = precios[servicio_seleccionado - 1];
     console.log(servicio);
      agregarAlCarrito(servicio);
      console.log("se agregó el servicio al carrito.")
-     salir = (prompt("desea contratar otro servicio?: (ESC para salir)")).toUpperCase();
+     salir = (prompt("desea contratar otro servicio?: (escriba ESC para salir)")).toUpperCase();
+
+
+
     }
-  }
-
- const serviciosContratados = [];
-
-  function agregarAlCarrito(servicio) {
-    let nuevo_serviciocontratado = new servicios (servicio);
-    serviciosContratados.push (nuevo_serviciocontratado);
-    
-
 }
+
+
+
+
+/*función que agrega la compra efectuada en "comprarServicio () a un carrito de compras." */
+
+function agregarAlCarrito(servicio) {
+    let nuevo_serviciocontratado = new servicios (servicio);
+    servicios_Contratados.push (nuevo_serviciocontratado);
+    }
+
+
+/*array que almacena las contrataciones que se han efetuado. */
+ 
+ let servicios_Contratados = [];
+ 
+
+
+/*función para mostrar, a través de un alert, el listado con la compra y el total a pagar */
+
 
 function mostrarCarrito (){
     let salida = "servicios contratados: \n\n";
     let total_pagar = 0;
 
-for (let servicio of serviciosContratados) {
-    salida += "Nombre: " + servicio.servicio + " Precio: " + servicio.precio + "\n";
-    total_pagar += parseFloat (servicio.precio.replace(".", "").replace(".", ""))
+for (let servicio of servicios_Contratados) {
+    salida += "Nombre: " + servicio.servicio + " Precio: $" + servicio.precio + "\n";
+    total_pagar += parseFloat (servicio.precio);
 }
 
-salida += "total a pagar: " + total_pagar.format(2 ,3 , '.', ',');
+salida += "total a pagar: $" + total_pagar;
 alert(salida);
-    console.log ("se listaron los servicios contratados");
+    
 }
 
 
-Number.prototype.format = function (x, n, s,c) {
-    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
-    num = this.toFixed(Math.max(0, ~~n));
-
-    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ',')); 
-};
-
-
-
+/*simulo la compra de un servicio, cargando mis datos y muestro la compra en un alert */
+solicitarConsulta();
 comprarServicio();
-mostrarCarrito(); 
-
-
-
-
+mostrarCarrito();
 
 
 
@@ -165,6 +169,3 @@ mostrarCarrito();
 
     
  
-
-
-   
