d1de934f5bf0c251db20fade6d8c83df32b16ecd
Matías <goffimatias@gmail.com>
2022-05-11 16:52:52 -0300

    evento aplicado al botonHire de mi portfolio

--- archivos js/portfolio.js
+++ archivos js/portfolio.js
@@ -1,16 +1,47 @@
+let portfolio = [ 
+  {id: 1, servicio:"Event up to 150 people", imagen:"/imagenes/casamiento.jpg", descripcion:"wedding coverage." }, 
+  {id: 2, servicio: "Event up to 50 people", imagen:"/imagenes/eventoLaboral.jpg", descripcion:"A work Party."},
+  {id: 3, servicio: "Event without limit of people", imagen:"/imagenes/conciertoMusical.jpg", descripcion:"Movistar arena concert coverage."},
+  {id: 4, servicio: "Basic - Individual Session", imagen:"/imagenes/portfolioLaboral.jpg", descripcion:"Christopher´s personal CV."},
+  {id: 5, servicio: "Book - Individual Session", imagen:"/imagenes/fiestade15.jpg", descripcion:"Lucy´s 15th birthday."},
+  {id: 6, servicio: "Street - Individual Session", imagen:"/imagenes/urbana.jpg", descripcion:"Joe´s Urban photo."},  
+];
+
+
 
 //creando a través del dom una etiqueta div con el elemento Card para volcar luego mi array con cada uno de los servicios ofrecidos
 
-let div = document.getElementById ("cards");
-let Card = document.createElement ("div");
-Card.classList.add("card");
+let div = document.getElementById ("container");
+portfolio.forEach((servicio) => {
+  let Card = document.createElement ("div");
+Card.classList.add("card", "text-white", "bg-dark","col-sm-12", "col-md-6", "col-lg-4");
 Card.innerHTML= `
-<img src="../imagenes/evento.jpg.jpg" class="card-img-top" alt="...">
+<img src="${servicio.imagen}" class="card-img-top  imagenPortfolio" alt="...">
 <div class="card-body">
-  <h5 class="card-title">Event</h5>
-  <p class="card-text">Wedding of 150 people.</p>
-  <a href="../paginas/presupuesto.html" class="btn btn-primary">Get a budget</a>
+  <h5 class="card-title">${servicio.servicio}</h5>
+  <p class="card-text">${servicio.descripcion}</p>
+  <a href="#" id="btnHire" class="btn btn-primary">Hire Service</a>
 </div>`;
 div.appendChild(Card);
 
-//no pude aún hacer el agregado de los styles! para especificar el espacio que ocupa en la grilla.
+
+});
+
+//creo carrito donde almaceno los servicios contratados
+
+let ServiciosContratados = [];
+
+
+
+let botonHire = document.getElementById ("btnHire");
+
+botonHire.onclick = () =>{
+  ServiciosContratados.push();
+  console.log (ServiciosContratados.length);
+  alert("servicio contratado!");
+}
+
+ 
+
+
+
